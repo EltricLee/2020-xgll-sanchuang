47e8a8f4a6b837711a5315092c206186caad958d
EltricLee <1024493607@qq.com>
2020-07-11 15:05:30 +0800

    配置文件修改

---
+++ flask_project_ex/config/settings.py
@@ -0,0 +1,5 @@
+DEBUG = True
+ENV = "development"
+
+HOST = "0.0.0.0"
+PORT = 8000
--- flask_project_ex/server.py
+++ flask_project_ex/server.py
@@ -6,10 +6,14 @@ from flask import Flask
 #字符串用来Flask对象标识，通常用__name__
 app = Flask(__name__)
 
+#加载配置
+app.config.from_object('config.settings')
+app.config.from_object('config.secure')
+
 #注册路由/index/
 #支持正则
 #app.router(路由规则)
-@app.route("/index/")
+@app.route("/index/",methods=["POST"],endpoint="index001")
 def index():
 	return "this is index"
 
@@ -18,6 +22,10 @@ def index2():
 app.add_url_rule('/index2/',view_func=index2)
 
 #运行app
-
-app.run()
+#debug=True =>热启动 =》代码做了修改，自动重启
+app.run(
+	host=app.config['HOST'],
+	port=app.config['PORT'],
+	debug=app.config['DEBUG'],
+)
 
